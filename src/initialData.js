893e3d903d71b85c1f1731aa84988d7ba8aeafb3
Tonux <sambndongo@gmail.com>
2020-11-08 14:19:58 +0000

    first commit

---
+++ src/initialData.js
@@ -0,0 +1,8 @@
+let initialData = [
+    {id: 'jsertu7a', name: 'Finir la formation React', completed: false},
+    {id: 'jseruo7l', name: 'Hotfix sur le projet au bureau', completed: false},
+    {id: 'jseruy2q', name: 'Signer contrat', completed: false},
+    {id: 'jserv4sw', name: 'Aller à la piscine', completed: false}
+]
+
+export default initialData;
